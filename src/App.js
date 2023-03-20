e2f2f08b03ae07cdaf8c087b3b49d52455f6afcf
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-21 01:53:35 +0200

    ref

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import AuthorizePage from "./pages/AuthorizePage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
-import { dec, inc } from "./redux/countSlice";
 
 function App() {
-  const count = useSelector((state) => state.count.counter);
-  const dispatch = useDispatch();
-  const foo = () => {
-    dispatch(inc());
-  };
-  const footoo = () => {
-    dispatch(dec());
-  };
-
   return (
     <div className="App">
-      <p>{count}</p>
-      <button onClick={foo}>+</button>
-      <button onClick={footoo}>-</button>
-      {/* <p>{counts}</p> */}
-      {/* <Routes> */}
-      {/* <Route path="/" element={<HomePage />} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<AuthorizePage />} />
-        <Route path="/register" element={<RegisterPage />} /> */}
-      {/* </Routes> */}
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import countSlice from "./countSlice";
 import userSlice from "./userSlice";
 
 export const store = configureStore({
   reducer: {
-    // userReducer: userSlice.reducer,
-    count: countSlice.reducer,
+    user: userSlice.reducer,
   },
 });
