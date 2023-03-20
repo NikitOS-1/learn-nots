590c7c8d2ee8a439a7f6516278060dc722ef19a2
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-21 01:51:59 +0200

    work counter

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import countSlice from "./countSlice";
 import userSlice from "./userSlice";
 
 export const store = configureStore({
   reducer: {
-    userReducer: userSlice.reducer,
+    // userReducer: userSlice.reducer,
+    count: countSlice.reducer,
   },
 });
