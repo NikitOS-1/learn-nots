0e8cb16098e4722b3808baebe2da0a6deb5b66ca
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-21 14:27:52 +0200

    add config firebase

--- src/pages/AuthorizePage.jsx
+++ src/pages/AuthorizePage.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import Form from "../components/Form";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const AuthorizePage = () => {
-  const handleLogin = (email, pass) => {};
+  const handleLogin = (email, pass) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, pass)
+      .then((state) => {
+        console.log(state);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <Form title="Login" handleClick={handleLogin} />
