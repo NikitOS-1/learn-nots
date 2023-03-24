32bc563deef2e920f6da524a8503be312d6f831f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-24 21:34:21 +0200

    ..

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { removeUser } from "../redux/userSlice";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
