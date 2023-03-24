9cb733fc20ec7c6544931c8a0484304aa92128bb
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-24 22:08:56 +0200

    .

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,14 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { onAuthStateChanged } from "firebase/auth";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const fetchUserData = createAsyncThunk(
-  "user/fetchUserData",
-
-  onAuthStateChanged(auth, (user) => {
-    console.log(user);
-  })
-);
 
 const initialState = {
   email: null,
@@ -33,7 +24,6 @@ const userSlice = createSlice({
       localStorage.clear();
     },
   },
-  extraReducers: (builder) => {},
 });
 
 export const { addUser, removeUser } = userSlice.actions;
