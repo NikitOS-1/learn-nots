213912ec4ff735b7d44439959c6b684d0e16dcbc
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-27 17:18:55 +0300

    add onAuthStateChanged and ref RequireAuth

--- src/Auth/RequireAuth.jsx
+++ src/Auth/RequireAuth.jsx
@@ -1,13 +1,22 @@
 import { Navigate, useLocation } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const RequireAuth = ({ children }) => {
   const location = useLocation();
-  const auth = localStorage.getItem("isLogin");
+  // const auth = localStorage.getItem("isLogin");
+  const auth = getAuth();
 
-  if (!auth) {
+  const user = auth.currentUser;
+  if (!user) {
     return <Navigate to={"/login"} state={{ from: location }} />;
+  } else {
+    return children;
   }
 
-  return children;
+  // if (!auth) {
+  //   return <Navigate to={"/login"} state={{ from: location }} />;
+  // }
+
+  // return children;
 };
 export { RequireAuth };
--- src/pages/Comp1.jsx
+++ src/pages/Comp1.jsx
@@ -1,15 +1,29 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserData } from "../redux/userDataSlice";
+// import { fetchUserIsLogin } from "../redux/userIsLogin";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Comp1 = () => {
   const { email, id, tokken } = useSelector((state) => state.user);
   const { status, error, data } = useSelector((state) => state.userData);
+  // const isLogined = useSelector((state) => state.userLogin.isLogin);
   const dispatch = useDispatch();
 
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log(user.uid);
+      console.log(user.email);
+      console.log(user.displayName);
+    } else {
+      console.log("no auth");
+    }
+  });
   useEffect(() => {
     dispatch(fetchUserData());
   }, [dispatch]);
+
   return (
     <div>
       Comp1 + email:{email}
