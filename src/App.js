291d8c5464673f217301b0db0f003ed66db2c560
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-04-17 18:24:14 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import { Route, Routes } from "react-router-dom";
-import { RequireAuth } from "./components/PrivatePages/RequireAuth";
-import AuthorizePage from "./pages/AuthorizePage";
-import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
-import Comp1 from "./pages/Comp1";
-import Comp2 from "./pages/Comp2";
 import { useDispatch, useSelector } from "react-redux";
 import { Switch } from "@mui/material";
 import "./App.style.scss";
 import { isDay } from "./redux/dayOrNightSlice";
-function App() {
-  const dispatch = useDispatch();
-  const checked = useSelector((state) => state.theme.theme);
 
+function App() {
+  const checked = useSelector((state) => state.theme.theme);
+  const dispatch = useDispatch();
   const handleChange = () => {
     checked ? dispatch(isDay(false)) : dispatch(isDay(true));
   };
+
   return (
     <div className={!checked ? "day" : "night"}>
       <Switch checked={checked} onChange={handleChange} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <RequireAuth>
-              <HomePage />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/comp1"
-          element={
-            <RequireAuth>
-              <Comp1 />
-            </RequireAuth>
-          }
-        />
-        <Route path="/comp2" element={<Comp2 />} />
-        <Route path="/login" element={<AuthorizePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
     </div>
   );
 }
