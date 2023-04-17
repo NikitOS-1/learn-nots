7c7e2a888ff87685436a693097eec36ec9cac3fa
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-04-17 18:17:02 +0300

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-
 const HomePage = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      <p> Welcome !!! </p>
-      <p> Home page</p>
-
-      <button>Log Out with {"email"}</button>
-      <Link to={"/comp1"}>to comp1</Link>
-      <br />
-      <Link to={"/comp2"}>to comp2</Link>
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 export default HomePage;
