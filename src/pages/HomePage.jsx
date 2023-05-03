6ba8c399bc2d8dc864b9fc486421f2605925b078
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-05-04 00:17:43 +0300

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 import React, { Suspense } from "react";
 
 const HomePage = () => {
   const database = getDatabase();
+  function writeUserData(userId, name, email) {
+    const db = getDatabase();
+    set(ref(db, "users/" + userId), {
+      username: name,
+      email: email,
+    });
+  }
+  writeUserData(
+    "cApY8wianOh1pzOYnyBq50BJoO22",
+    "nikita",
+    "nik@LogoDevSharp.fd"
+  );
   console.log(database);
   return (
     <div>
