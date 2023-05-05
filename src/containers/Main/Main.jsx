65008d80ebffecf794696d09a11f7fb5e93ecc9b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-05-05 23:51:44 +0300

    comp structure

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,4 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/HomePage";
+import SignIn from "../../pages/SignIn";
+import SignUp from "../../pages/SignUp";
+
 const Main = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </main>
+  );
 };
 export default Main;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 import { getDatabase, ref, set } from "firebase/database";
 import React, { Suspense, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const HomePage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/sign-in");
+      }
+    });
+  });
+  return <div>Home</div>;
 };
 export default HomePage;
