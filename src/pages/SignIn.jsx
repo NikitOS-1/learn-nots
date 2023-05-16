bc693089be1bb5ee32744f9d1d98ac52f4ce9835
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-05-17 00:09:40 +0300

    add observer

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import FormSignIn from "../components/Form/FormSignIn";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/");
+      }
+    });
+  });
   return <FormSignIn />;
 };
 export default SignIn;
