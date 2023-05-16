7267b013b384e0260ec404324b42e5371699c251
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-05-17 00:25:59 +0300

    ref

--- src/components/Form/FormSignIn.jsx
+++ src/components/Form/FormSignIn.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect, useState } from "react";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const FormSignIn = () => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+  const [error, setError] = useState("");
+
   const auth = getAuth();
+
+  const navigate = useNavigate();
 
   const submit = (e) => {
     e.preventDefault();
@@ -17,28 +26,38 @@ const FormSignIn = () => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorMessage);
+        setError(errorMessage);
       });
   };
 
-  return (
-    <form typeof="submit">
-      <h1>SignIn</h1>
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail((prev) => (prev = e.target.value))}
-      />{" "}
-      <br />
-      <input
-        type="password"
-        value={pass}
-        onChange={(e) => setPass((prev) => (prev = e.target.value))}
-      />{" "}
-      <br />
-      <button onClick={submit}>SignIn</button>
-    </form>
-  );
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/");
+      } else {
+        return (
+          <form typeof="submit">
+            <h1>SignIn</h1>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail((prev) => (prev = e.target.value))}
+            />{" "}
+            <br />
+            <input
+              type="password"
+              value={pass}
+              onChange={(e) => setPass((prev) => (prev = e.target.value))}
+            />{" "}
+            <br />
+            {error}
+            <br />
+            <button onClick={submit}>SignIn</button>
+          </form>
+        );
+      }
+    });
+  });
 };
 
 export default FormSignIn;
