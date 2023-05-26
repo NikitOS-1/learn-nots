5e63aa41103932281ae533e8e0c955821ea381e0
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-05-26 11:02:31 +0300

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,8 +31,6 @@ const HomePage = () => {
     onAuthStateChanged(auth, (user) => {
       if (!user) {
         navigate("/sign-in");
-        navigate("/sign-in");
-        navigate("/sign-in");
       }
     });
   });
