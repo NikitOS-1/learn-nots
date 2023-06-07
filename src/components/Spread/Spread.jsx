f9b661db70b6889af704a6cb5ea64a6d014e8da2
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-07 20:55:32 +0300

    concat

--- src/components/Spread/Spread.jsx
+++ src/components/Spread/Spread.jsx
@@ -1,4 +1,10 @@
 const Spread = () => {
+  // const number1 = [1, 2, 3, 4, 5];
+  // const number2 = [7, 8, 9];
+
+  // const allNumbers = [83, ...number1, ...number2];
+  // console.log(allNumbers);
+
   return <div>Spread</div>;
 };
 export default Spread;
