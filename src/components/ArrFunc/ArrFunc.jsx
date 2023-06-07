5eb29a40fa9ff7785ba86960e7624d7902047067
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-07 19:50:33 +0300

    arr func

---
+++ src/components/ArrFunc/ArrFunc.jsx
@@ -0,0 +1,16 @@
+const ArrFunc = () => {
+  //   function sum(a, b) {
+  //     return a + b;
+  //   }
+  // const sum2 = function(a,b){
+  //     return a+b
+  // }
+  //   -----ES6
+  const sum = (a, b) => {
+    a + b;
+  };
+  const sum2 = (a, b) => a + b;
+
+  return <div>ArrFunc</div>;
+};
+export default ArrFunc;
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -3,10 +3,12 @@ import HomePage from "../../pages/HomePage";
 import SignIn from "../../pages/SignIn";
 import SignUp from "../../pages/SignUp";
 import { RequireAuth } from "../../hooks/PrivatePages/RequireAuth";
+import ArrFunc from "../../components/ArrFunc/ArrFunc";
 
 const Main = () => {
   return (
     <main>
+      <ArrFunc />
       <Routes>
         {/* <Route path="/" element={<RequireAuth children={<HomePage />} />} />
         <Route path="/sign-in" element={<SignIn />} />
