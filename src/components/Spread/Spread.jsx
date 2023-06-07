de156c22ff5d75c823bd584953606c00d24d4122
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-07 21:04:58 +0300

    obj clone and merge

--- src/components/Spread/Spread.jsx
+++ src/components/Spread/Spread.jsx
@@ -4,7 +4,20 @@ const Spread = () => {
 
   // const allNumbers = [83, ...number1, ...number2];
   // console.log(allNumbers);
-
+  // function sum(a, b, c) {
+  //   return a + b + c;
+  // }
+  // sum(...number1);
+  const obj1 = {
+    name: "Anna",
+    x: 20,
+  };
+  const obj2 = {
+    name: "Bory",
+    x: 99,
+  };
+  const newObjClone = { ...obj1 };
+  const mergedObj = { ...obj1, ...obj2 };
   return <div>Spread</div>;
 };
 export default Spread;
