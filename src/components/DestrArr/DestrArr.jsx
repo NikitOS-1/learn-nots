a022d6256ce9fad13b1436bb2f73f90ea5ae264b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-08 19:15:38 +0300

    .

--- src/components/DestrArr/DestrArr.jsx
+++ src/components/DestrArr/DestrArr.jsx
@@ -1,6 +1,6 @@
 const DestrArr = () => {
-  const number = [1, 2, 3, 4, 5];
-  const [first, second, third, ...tail] = number || [];
+  const number = [1, 2, [44, 33], 3, 4, 5];
+  const [first, second, [arr1, arr2], third, ...tail] = number || [];
 
   return <div>DestrArr</div>;
 };
