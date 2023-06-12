c87732e834859afc9c215ca77c9e750a662a3231
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 23:37:18 +0300

    recurse() with cond

--- src/components/Recurse/Recurse.jsx
+++ src/components/Recurse/Recurse.jsx
@@ -1,10 +1,16 @@
 import { clearAllListeners } from "@reduxjs/toolkit";
 
 const Recurse = () => {
+  let count = 0;
   function recurse() {
+    if (count === 5) {
+      return console.log("first");
+    }
+    count++;
     recurse();
   }
   console.log(recurse());
+
   return <div>Recurse</div>;
 };
 export default Recurse;
