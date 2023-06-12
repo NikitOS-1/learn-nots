ae3d1abd0825302105df730c510b67b026d982c0
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 23:29:36 +0300

    .

--- src/components/Recurse/Recurse.jsx
+++ src/components/Recurse/Recurse.jsx
@@ -1,4 +1,10 @@
+import { clearAllListeners } from "@reduxjs/toolkit";
+
 const Recurse = () => {
+  function recurse() {
+    recurse();
+  }
+  console.log(recurse());
   return <div>Recurse</div>;
 };
 export default Recurse;
