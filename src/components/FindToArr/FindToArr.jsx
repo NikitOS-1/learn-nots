87856ebc14024e035d153f452a8f55f9d7c97cac
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 21:01:38 +0300

    with find()

--- src/components/FindToArr/FindToArr.jsx
+++ src/components/FindToArr/FindToArr.jsx
@@ -3,23 +3,27 @@ const FindToArr = () => {
   const langs = ["HTML", "CSS", "JS", "Python", "PHP"];
   const basket = [
     {
+      id: 1,
       title: "Item1",
       price: 100,
       category: "product",
     },
     {
+      id: 2,
       title: "Item2",
       price: 200,
       category: "not",
     },
     {
+      id: 3,
       title: "Item3",
       price: 300,
       category: "product",
     },
   ];
-  const result = numbers.includes(9);
+  const result = basket.find((element) => element.category === "product");
   console.log("result", result);
+
   return <div>FindToArr</div>;
 };
 export default FindToArr;
