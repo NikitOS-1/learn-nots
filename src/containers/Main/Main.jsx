b1929ed0ce69d43aa6b83f754ee011623853544a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 20:18:27 +0300

    .

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,17 +1,6 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "../../pages/HomePage";
-import SignIn from "../../pages/SignIn";
-import SignUp from "../../pages/SignUp";
-import { RequireAuth } from "../../hooks/PrivatePages/RequireAuth";
-import ArrFunc from "../../components/ArrFunc/ArrFunc";
-import Spread from "../../components/Spread/Spread";
-import DestrArr from "../../components/DestrArr/DestrArr";
 import DestrObj from "../../components/DestrObj/DestrObj";
 
 const Main = () => {
-  const array = [7, "correct", 0, false, 9, NaN, ""];
-  const notFalsy = array.filter((n) => !!n);
-  console.log(notFalsy);
   return (
     <main>
       <DestrObj />
