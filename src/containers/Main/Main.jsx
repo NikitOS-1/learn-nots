e02b14675b99da03a850823a52ab8c4f29b61f87
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 20:18:09 +0300

    with arr method filter

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -10,12 +10,7 @@ import DestrObj from "../../components/DestrObj/DestrObj";
 
 const Main = () => {
   const array = [7, "correct", 0, false, 9, NaN, ""];
-  const notFalsy = [];
-  for (let i = 0; i < array.length; i++) {
-    if (!!array[i]) {
-      notFalsy.push(array[i]);
-    }
-  }
+  const notFalsy = array.filter((n) => !!n);
   console.log(notFalsy);
   return (
     <main>
