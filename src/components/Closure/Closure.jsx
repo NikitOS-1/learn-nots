773edc0674c7157b7d5b43c305f8678811c633b3
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-13 00:10:26 +0300

    Closure

--- src/components/Closure/Closure.jsx
+++ src/components/Closure/Closure.jsx
@@ -1,4 +1,12 @@
 const Closure = () => {
+  function createFn() {
+    function greeting(str) {
+      console.log("Hello " + str);
+    }
+    return greeting;
+  }
+  const hi = createFn();
+  console.log(hi("foo"));
   return <div>Closure</div>;
 };
 export default Closure;
