52f2b6f59bb72fb78dd70dd5058bd3e2c1e6cb11
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 20:08:46 +0300

    arr reverse()

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -9,40 +9,8 @@ import DestrArr from "../../components/DestrArr/DestrArr";
 import DestrObj from "../../components/DestrObj/DestrObj";
 
 const Main = () => {
-  // let arr = [9, 3, 2, 8, 5, 6, 7, 4, 1];
-
-  // let sortArr = arr.sort((x1, x2) => {
-  //   if (x1 < x2) {
-  //     return -1;
-  //   }
-  //   if (x1 === x2) {
-  //     return 0;
-  //   }
-  //   return 1;
-  // });
-  // console.log(sortArr);
-
-  const users = [
-    {
-      name: "Djon",
-      age: 12,
-    },
-    {
-      name: "akol",
-      age: 10,
-    },
-    {
-      name: "fQwor",
-      age: 14,
-    },
-  ];
-  let sortUsersToStr = users.sort((a, b) => {
-    return a.name.localeCompare(b.name);
-  });
-  console.log(sortUsersToStr);
-  // let sortUsersToNumb = users.sort((a, b) => a.age - b.age);
-  // console.log(users);
-  // console.log(sortUsersToNumb);
+  let arr = [9, 3, 2, 8, 5, 6, 7, 4, 1];
+  console.log(arr.reverse());
 
   return (
     <main>
