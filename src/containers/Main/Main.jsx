c2c980583b9eeb16a0922565db56fa95db3206ee
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-12 19:15:46 +0300

    find min ot max number in arr

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -10,7 +10,13 @@ import DestrObj from "../../components/DestrObj/DestrObj";
 
 const Main = () => {
   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < max) {
+      max = arr[i];
+    }
+  }
+  console.log(max);
   return (
     <main>
       <DestrObj />
