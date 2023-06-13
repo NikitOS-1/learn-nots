d60c6a30ec265ae181b74b5b291b5cbd79b640fe
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-13 21:43:38 +0300

    asynk func

--- src/components/AsyncFn/AsyncFn.jsx
+++ src/components/AsyncFn/AsyncFn.jsx
@@ -1,4 +1,8 @@
 const AsyncFn = () => {
+  fetch("https://jsonplaceholder.typicode.com/t1odos/1")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch(() => console.error("Error now"));
   return <div>AsyncFn</div>;
 };
 export default AsyncFn;
