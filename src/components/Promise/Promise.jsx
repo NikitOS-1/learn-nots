4997c4280d074edeec5760e37f8cb95ba084654c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-13 17:19:30 +0300

    promise

---
+++ src/components/Promise/Promise.jsx
@@ -0,0 +1,12 @@
+const Promise1 = () => {
+  const promice1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("foo");
+    }, 1000);
+  });
+  promice1.then((value) => {});
+  promice1.catch((value) => {});
+  console.log(promice1);
+  return <div>Promise</div>;
+};
+export default Promise1;
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,12 +1,14 @@
 import Closure from "../../components/Closure/Closure";
 import DestrObj from "../../components/DestrObj/DestrObj";
 import FindToArr from "../../components/FindToArr/FindToArr";
+import Promise1 from "../../components/Promise/Promise";
 import Recurse from "../../components/Recurse/Recurse";
 
 const Main = () => {
   return (
     <main>
-      <Closure />
+      <Promise1 />
+      {/* <Closure /> */}
       {/* <Recurse /> */}
       {/* <FindToArr /> */}
       {/* <DestrObj /> */}
