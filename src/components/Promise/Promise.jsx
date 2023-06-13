2b98491ad9dec4d477700a37c5e3d2bef2bbd67e
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-13 21:38:03 +0300

    fetch()

--- src/components/Promise/Promise.jsx
+++ src/components/Promise/Promise.jsx
@@ -1,14 +1,21 @@
 const Promise1 = () => {
-  const promice1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("foo");
-    }, 1000);
-  });
-  promice1.then((value) => {
-    console.log(value);
-  });
-  promice1.catch((value) => {});
-  console.log(promice1);
+  //   const promice1 = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve("foo");
+  //     }, 1000);
+  //   });
+  //   promice1.then((value) => {
+  //     console.log(value);
+  //   });
+  //   promice1.catch((value) => {});
+  //   console.log(promice1);
+  fetch("https://jsonplaceholder.typicode.com/todos1/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch(() => {
+      let error = new Error("ERROR");
+      console.error(error);
+    });
   return <div>Promise</div>;
 };
 export default Promise1;
