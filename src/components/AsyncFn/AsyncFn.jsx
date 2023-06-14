d1b24385699544e37bd4a26bfa9716db39dc0d0f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-14 16:31:12 +0300

    .

--- src/components/AsyncFn/AsyncFn.jsx
+++ src/components/AsyncFn/AsyncFn.jsx
@@ -1,19 +1,23 @@
 const AsyncFn = () => {
-  let url = "https://jsonplaceholder.typicode.com/todos/1";
+  let url = "https://jsonplaceholder.typicod3e.com/todos/1";
   //   fetch(url)
   //     .then((res) => res.json())
   //     .then((data) => console.log(data))
   //     .catch(console.error("Error now"));
 
-  //   async function getPosts(url) {
-  //     const response = await fetch(url);
-  //     const post = await response.json();
-
-  //     return console.log(post);
-  //   }
-  //   getPosts(url);
+  async function getPosts(url) {
+    try {
+      const response = await fetch(url);
+      const post = await response.json();
+      return console.log(post);
+    } catch (error) {
+      console.error(error.message);
+      return error;
+    }
+  }
+  getPosts(url);
   //
-  const getComments = async () => {};
+  //   const getComments = async () => {};
   //
 
   return <div>AsyncFn</div>;
