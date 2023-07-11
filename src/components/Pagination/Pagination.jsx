d445cea1b132116d70e29a8442c72198b5b3e80d
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-11 10:16:39 +0300

    pagination with get response on HN

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const BASE_URL = "http://hn.algolia.com/api/v1/search?";
+
+const Pagination = () => {
+  const [post, setPost] = useState([]);
+
+  return <div>Pagination</div>;
+};
+export default Pagination;
