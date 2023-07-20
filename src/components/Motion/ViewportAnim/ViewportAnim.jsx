56466d169aef724acf836f4fd466bb943709face
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-20 20:47:50 +0300

    ref

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,19 +1,4 @@
 const ViewportAnim = () => {
-  const array = [[10, 1, [2, 11]], [1], [4, 1]];
-
-  let onesArray = [];
-
-  function flat(arr) {
-    arr.forEach((el) => {
-      if (Array.isArray(el)) {
-        flat(el);
-      } else {
-        onesArray.push(el);
-      }
-    });
-    return onesArray;
-  }
-  console.log(flat(array).reduce((acc, el) => acc + el, 0));
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
--- src/components/Recurse/Recurse.jsx
+++ src/components/Recurse/Recurse.jsx
@@ -1,6 +1,7 @@
 import { clearAllListeners } from "@reduxjs/toolkit";
 
 const Recurse = () => {
+  // --------------------------------
   let count = 0;
   function recurse() {
     if (count === 5) {
@@ -10,7 +11,23 @@ const Recurse = () => {
     recurse();
   }
   console.log(recurse());
+  // --------------------------------
+  const array = [[10, 1, [2, 11]], [1], [4, 1]];
 
+  let onesArray = [];
+
+  function flat(arr) {
+    arr.forEach((el) => {
+      if (Array.isArray(el)) {
+        flat(el);
+      } else {
+        onesArray.push(el);
+      }
+    });
+    return onesArray;
+  }
+  console.log(flat(array).reduce((acc, el) => acc + el, 0));
+  // --------------------------------
   return <div>Recurse</div>;
 };
 export default Recurse;
