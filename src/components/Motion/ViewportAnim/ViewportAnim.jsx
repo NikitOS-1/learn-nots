e490acc96b5aea6cdca053c8411f4600161a749c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-20 21:13:50 +0300

    .

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,14 +1,20 @@
 const ViewportAnim = () => {
-  for (var i = 0; i < 100; i++) {
-    setTimeout(
-      (n) => {
-        console.log(n);
-      },
-      0,3
-      i
-    );
-  }
+  //   console.log(1);
 
+  //   const a = new Promise((resolve, reject) => resolve(console.log(2)));
+
+  //   a.then((res) => console.log(3));
+
+  //   setTimeout(() => {
+  //     console.log(4);
+  //   }, 0);
+
+  //   console.log(5);
+  // //1
+  // //2
+  // //5
+  // //3
+  // //4
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
