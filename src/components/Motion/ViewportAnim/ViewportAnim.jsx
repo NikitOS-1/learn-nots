feb17aa9af11b8702f32a2728fa860bd0094c630
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-20 20:33:43 +0300

    recurs func for arr

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,4 +1,18 @@
 const ViewportAnim = () => {
+  const array = [[1, 1, [1, 1]], [1], [1, 1]];
+  let onesArray = [];
+
+  function flat(arr) {
+    arr.forEach((el) => {
+      if (Array.isArray(el)) {
+        flat(el);
+      } else {
+        onesArray.push(el);
+      }
+    });
+    return onesArray;
+  }
+  console.log(flat(array));
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
