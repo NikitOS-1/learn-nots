76d0c3f72fa35807bc4b9c8e9d2bc9bbdf10f99c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-20 20:57:48 +0300

    .

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,4 +1,14 @@
 const ViewportAnim = () => {
+  for (var i = 0; i < 100; i++) {
+    setTimeout(
+      (n) => {
+        console.log(n);
+      },
+      0,3
+      i
+    );
+  }
+
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
