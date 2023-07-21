46e8353d09f0090134b347847c54934f4ebcaf2f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-21 18:28:55 +0300

    .

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,4 +1,9 @@
 const ViewportAnim = () => {
+  let arr = [1, 2, 3, 4, 5];
+  for (let item of arr) {
+    console.log(item);
+  }
+
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
