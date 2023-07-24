e81fcc2d90aa82467f7eb879265600b7920c09bf
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-24 20:15:35 +0300

    add comp

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,23 +1,4 @@
 const ViewportAnim = () => {
-  const item = {
-    name: "Car",
-    color: "white",
-    type: "2.0",
-    brand: "hyundai",
-    getBrand: () => console.log(item.brand),
-    variants: [1, 2, 3, 4, { fn: () => console.log("fn") }],
-  };
-  for (let key in item) {
-    delete item[key];
-  }
-  const copyItem = {};
-  console.log(item);
-  // for (let key in item) {
-  //   copyItem[key] = item[key];
-  // }
-
-  // Object.assign(copyItem, item);
-
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
