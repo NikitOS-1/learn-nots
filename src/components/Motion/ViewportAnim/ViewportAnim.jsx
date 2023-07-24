78bedf3781929354369c09fa58436c65da13042e
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-24 18:44:45 +0300

    test

--- src/components/Motion/ViewportAnim/ViewportAnim.jsx
+++ src/components/Motion/ViewportAnim/ViewportAnim.jsx
@@ -1,4 +1,17 @@
 const ViewportAnim = () => {
+  const item = {
+    name: "Car",
+    color: "white",
+    type: "2.0",
+    brand: "hyundai",
+    getBrand: () => console.log(item.brand),
+    variants: [1, 2, 3, 4, { fn: () => console.log("fn") }],
+  };
+
+  const jsonData = JSON.stringify(item);
+  const getJsonData = JSON.parse(jsonData);
+  console.log(jsonData);
+
   return <div>ViewportAnim</div>;
 };
 export default ViewportAnim;
