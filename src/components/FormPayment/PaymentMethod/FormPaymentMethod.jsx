4eb8a2398c148b9e11cfbace30d770dc65b970b2
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-12 00:10:01 +0300

    .

---
+++ src/components/FormPayment/PaymentMethod/FormPaymentMethod.jsx
@@ -0,0 +1,92 @@
+import Errors from "../Errors";
+import visaMasterCard from "../visa-master.png";
+import "./FormPaymentMethod.scss";
+
+const FormPaymentMethod = (errors, register) => {
+  return (
+    <div className="payment-method">
+      <h3>Payment Method</h3>
+      <div className="payment-type">
+        <select className="payment-select">
+          <option value="PayPal Checkout">PayPal Checkout</option>
+          <option value="Stripe">Stripe</option>
+        </select>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed
+          ducimus, deserunt .
+        </p>
+      </div>
+      <div className="check-payment-system">
+        <img src={visaMasterCard} alt="payment" />
+      </div>
+      <label className="accountNumber">
+        <input
+          type="text"
+          {...register("accountNumber", {
+            required: "Enter your account number",
+            minLength: {
+              value: 16,
+              message: "Invalid card",
+            },
+          })}
+        />
+        <Errors
+          errors={errors?.accountNumber}
+          errorMessage={errors?.accountNumber?.message}
+        />
+        <p>Card Number</p>
+      </label>
+      <div className="acc-cart-about">
+        <label className="expDate">
+          <input
+            type="text"
+            {...register("expDate", {
+              required: "Enter expiration Date",
+              minLength: {
+                value: 4,
+                message: "Invalid exp Date",
+              },
+            })}
+          />
+          <Errors
+            errors={errors?.expDate}
+            errorMessage={errors?.expDate?.message}
+          />
+          <p>Expiration Date</p>
+        </label>
+        <label className="svs">
+          <input
+            type="text"
+            {...register("svs", {
+              required: "Enter you SVS",
+              minLength: {
+                value: 3,
+                message: "Invalid SVS code",
+              },
+            })}
+          />
+          <Errors errors={errors?.svs} errorMessage={errors?.svs?.message} />
+          <p>Security Code</p>
+        </label>
+      </div>
+      <label className="cartName">
+        <input
+          type="text"
+          {...register("cartName", {
+            required: "Enter your Name",
+            minLength: {
+              value: 5,
+              message: "Invalid Name",
+            },
+          })}
+        />
+        <Errors
+          errors={errors?.cartName}
+          errorMessage={errors?.cartName?.message}
+        />
+        <p>Cardholder Name</p>
+      </label>
+    </div>
+  );
+};
+export default FormPaymentMethod;
