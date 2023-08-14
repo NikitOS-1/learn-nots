07e723ca4629f373a00acede7e54de055189cca2
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-14 21:49:28 +0300

    start burger menu

---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,13 @@
+const BurgerMenu = () => {
+  return (
+    <div>
+      <div className="app-container">
+        <button className="button-burger">Open</button>
+        <div className="content">
+          <h1>Burger menu </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default BurgerMenu;
