a3765c9d0596e0b07886ca6eb905621b3467f593
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-16 20:57:32 +0300

    add new videos

--- src/components/BgVideo/BgVideo.jsx
+++ src/components/BgVideo/BgVideo.jsx
@@ -4,11 +4,9 @@ const BgVideo = () => {
   return (
     <div className="video-bg">
       <div className="video-bg__inner">
-        <video
-          src="smokeVideo.mp4"
-          autoPlay="true"
-          loop
-          className="video-bg__video"></video>
+        <video autoPlay muted playsInline loop className="video-bg__video">
+          <source src="two-vid.mp4" type="video/mp4" />
+        </video>
       </div>
       <div className="video-bg__content">
         <h1>Video Background</h1>
