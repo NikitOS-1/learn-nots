599dd5f0a6b1e9b282b969b8468e9b83e24c3e68
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-07 18:52:24 +0300

    .

--- src/hooks/PrivatePages/RequireAuth.jsx
+++ src/hooks/PrivatePages/RequireAuth.jsx
@@ -5,6 +5,7 @@ export const RequireAuth = ({ children }) => {
   const location = useLocation();
   const auth = getAuth();
   const user = auth.currentUser;
+
   if (!user) {
     return <Navigate to={"/sign-in"} state={{ from: location }} />;
   } else {
