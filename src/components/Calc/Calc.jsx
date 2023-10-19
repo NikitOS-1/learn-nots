82c09599c8d33d47e38f93be6d7d62375770df3b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-19 20:44:00 +0300

    del

--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -1,14 +1,7 @@
 import { useState } from "react";
 
 const Calc = () => {
-  const [res, setRes] = useState(0);
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
-
-  const calc = (e) => {
-    let enter = e.target.value;
-    setRes(enter);
-  };
+  const calc = (e) => {};
   return <div>foo</div>;
 };
 export default Calc;
