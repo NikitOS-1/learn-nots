c73985a847fceb444b1605679a26db9c6f384d74
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-25 18:48:34 +0300

    .

--- src/components/tests/Tests.jsx
+++ src/components/tests/Tests.jsx
@@ -1,8 +1,4 @@
 const Tests = () => {
-  function solve(weight0, weight1, weight2) {
-    let allArray = [...weight0, ...weight1, ...weight2];
-    return allArray.sort((a, b) => a - b);
-  }
-  return solve([1, 2, 3], [48, 0, 123], [34, 32, 5]);
+  return <>test</>;
 };
 export default Tests;
