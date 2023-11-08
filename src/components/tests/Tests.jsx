9e480b6c1297f23456876050977ce452795ebc52
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-08 17:12:52 +0200

    .

--- src/components/tests/Tests.jsx
+++ src/components/tests/Tests.jsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 const Tests = () => {
-  return <>test</>;
+  const [word, setWord] = useState("...");
+  const [letter, setLetter] = useState("...");
+
+  return (
+    <>
+      <button>Click here</button>
+      <div>{letter}</div>
+      <div>{word}</div>
+    </>
+  );
 };
 export default Tests;
