b10363c6eb49735a01d86981402900148fbd47c3
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-23 17:22:44 +0200

    Types Data in JS

--- src/components/TS/someExamples.ts
+++ src/components/TS/someExamples.ts
@@ -1,4 +1,14 @@
-//basic types into variable
+// Types Data in JS
+// 1.null
+// 2.undefined
+// 3.number
+// 4.string
+// 5.boolean
+// 6.object
+// 7.BigInt
+// 8.Symbol
+
+// ------------basic types into variable----------------
 let age: number = 30;
 let names: string = "Nikita";
 let isDone: boolean = false;
@@ -6,6 +16,7 @@ let myNull: null = null;
 let myUndefined: undefined = undefined;
 let x: any = 30;
 
+// -----------------Function--------------------
 //function return type
 function foo(): void {
   console.log("something");
