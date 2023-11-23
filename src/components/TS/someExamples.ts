1337ed827e60f765d516e604d5f494410b0b0eda
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-23 17:20:10 +0200

    TS function with typed parameters and return type

--- src/components/TS/someExamples.ts
+++ src/components/TS/someExamples.ts
@@ -1,4 +1,17 @@
+//basic types into variable
 let age: number = 30;
-//aliase type
+let names: string = "Nikita";
+let isDone: boolean = false;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+let x: any = 30;
 
-//interface
+//function return type
+function foo(): void {
+  console.log("something");
+}
+
+//function with typed parameters and return type
+function add(x: number, y: number): number {
+  return x + y;
+}
