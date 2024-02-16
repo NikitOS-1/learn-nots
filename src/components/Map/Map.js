3eba95d7232ee11b42d53fdee5c3d22073abaa80
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-02-16 19:30:02 +0200

    feat(Map): data structure Map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -27,6 +27,37 @@ myMap.forEach((value, key) => {
   console.log(key + " = " + value);
 });
 
+function Login() {
+  return <div>Login</div>;
+}
+function About() {
+  return <div>About</div>;
+}
+function Contact() {
+  return <div>Contact</div>;
+}
+
+const componentsMap = new Map([
+  ["Login", <Login />],
+  ["About", <About />],
+  ["Contact", <Contact />],
+]);
+const [currentComponentKey, setCurrentComponentKey] = useState("Login");
+
+const goToNextPage = () => {
+  const keys = Array.from(componentsMap.keys());
+  const currentIndex = keys.indexOf(currentComponentKey);
+  const nextIndex = (currentIndex + 1) % keys.length;
+  setCurrentComponentKey(keys[nextIndex]);
+};
+
+return (
+  <div>
+    {componentsMap.get(currentComponentKey)}
+    <button onClick={goToNextPage}>Next</button>
+  </div>
+);
+
 // Инструменты для CI/CD:
 // Jenkins: Популярный инструмент для автоматизации процессов CI/CD.
 // GitLab CI/CD: Интегрированное решение для управления и автоматизации CI/CD процессов.
