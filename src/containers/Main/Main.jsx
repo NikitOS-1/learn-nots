7a2c7436082a479f12c72f3d88097d1b976b6222
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-04-03 19:14:50 +0300

    .

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,29 +1,29 @@
-import AsyncFn from "../../components/AsyncFn/AsyncFn";
-import Closure from "../../components/Closure/Closure";
-import DestrObj from "../../components/DestrObj/DestrObj";
-import FindToArr from "../../components/FindToArr/FindToArr";
-import Motion from "../../components/Motion/Motionbasic/Motion";
-import ShowHidden from "../../components/Motion/AnimatePresence/ShowHidden";
-import Pagination from "../../components/Pagination/Pagination";
-import Promise1 from "../../components/Promise/Promise";
-import Recurse from "../../components/Recurse/Recurse";
-import ReactHooKForm from "../../hooks/ReactHooKForm/ReactHooKForm";
-import AnimatePresences from "../../components/Motion/AnimatePresence/AnimatePresences";
-import FilterPages from "../../components/Motion/Filter/FilterPages";
-import Menu from "../../components/Motion/AnimateSharedLayout/Menu";
-import LazyLoad from "../../components/LazyLoad/LazyLoad";
-import LocalStorage from "../../components/LocalStorage/LocalStorage";
-import { Route, Routes, Link } from "react-router-dom";
-import { RequireAuth } from "../../hooks/PrivatePages/RequireAuth";
-import HomePage from "../../pages/HomePage";
-import SignIn from "../../pages/SignIn";
-import SignUp from "../../pages/SignUp";
-import Auth from "../../components/Auth/Auth";
-import UsedModal from "../../components/Modal/UsedModal";
-import Table from "../../components/Table/Table";
-import BurgerMenu from "../../components/BurgerMenu/BurgerMenu";
-import BgVideo from "../../components/BgVideo/BgVideo";
-import Tests from "../../components/tests/Tests";
+import AsyncFn from '../../components/AsyncFn/AsyncFn'
+import Closure from '../../components/Closure/Closure'
+import DestrObj from '../../components/DestrObj/DestrObj'
+import FindToArr from '../../components/FindToArr/FindToArr'
+import Motion from '../../components/Motion/Motionbasic/Motion'
+import ShowHidden from '../../components/Motion/AnimatePresence/ShowHidden'
+import Pagination from '../../components/Pagination/Pagination'
+import Promise1 from '../../components/Promise/Promise'
+import Recurse from '../../components/Recurse/Recurse'
+import ReactHooKForm from '../../hooks/ReactHooKForm/ReactHooKForm'
+import AnimatePresences from '../../components/Motion/AnimatePresence/AnimatePresences'
+import FilterPages from '../../components/Motion/Filter/FilterPages'
+import Menu from '../../components/Motion/AnimateSharedLayout/Menu'
+import LazyLoad from '../../components/LazyLoad/LazyLoad'
+import LocalStorage from '../../components/LocalStorage/LocalStorage'
+import { Route, Routes, Link } from 'react-router-dom'
+import { RequireAuth } from '../../hooks/PrivatePages/RequireAuth'
+import HomePage from '../../pages/HomePage'
+import SignIn from '../../pages/SignIn'
+import SignUp from '../../pages/SignUp'
+import Auth from '../../components/Auth/Auth'
+import UsedModal from '../../components/Modal/UsedModal'
+import Table from '../../components/Table/Table'
+import BurgerMenu from '../../components/BurgerMenu/BurgerMenu'
+import BgVideo from '../../components/BgVideo/BgVideo'
+import Tests from '../../components/tests/Tests'
 
 const Main = () => {
   return (
@@ -35,7 +35,7 @@ const Main = () => {
       </Routes> */}
       {/* <AsyncFn /> */}
       {/* <Promise1 /> */}
-      {/* <Closure /> */}
+      <Closure />
       {/* <Recurse /> */}
       {/* <FindToArr /> */}
       {/* <DestrObj /> */}
@@ -57,8 +57,8 @@ const Main = () => {
       {/* <Table /> */}
       {/* <BurgerMenu /> */}
       {/* <BgVideo /> */}
-      <Tests />
+      {/* <Tests /> */}
     </main>
-  );
-};
-export default Main;
+  )
+}
+export default Main
