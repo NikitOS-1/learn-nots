593561e3149be5f4fc9a592c51733ae714e1c945
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-04-08 23:52:34 +0300

    .

--- src/components/Closure/Closure.jsx
+++ src/components/Closure/Closure.jsx
@@ -1,12 +1,22 @@
-const Closure = () => {
-  function createFn() {
-    function greeting(str) {
-      console.log("Hello " + str);
-    }
-    return greeting;
-  }
-  const hi = createFn();
-  console.log(hi("foo"));
-  return <div>Closure</div>;
-};
-export default Closure;
+// const Closure = () => {
+//   function createFn() {
+//     function greeting(str) {
+//       console.log("Hello " + str);
+//     }
+//     return greeting;
+//   }
+//   const hi = createFn();
+//   console.log(hi("foo"));
+//   return <div>Closure</div>;
+// };
+// export default Closure;
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const filterArr = () => {
+  let filterArray = []
+  arr.filter((i) => (i % 0 ? filterArr.push[i] : false))
+  return filterArr
+}
+
+filterArr()
