418b49c69cd06a85f546574a0e00452f636c16a4
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-04-09 17:26:55 +0300

    add filter

--- src/components/Closure/Closure.jsx
+++ src/components/Closure/Closure.jsx
@@ -15,11 +15,12 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const filterArray = (arr) => {
   let newArray = []
-  for (let i = 0; i < arr.lenght; i++) {
-    if (arr[i] % 2 === 0) {
-      newArray.push(arr[i])
-    }
-  }
+  // for (let i = 0; i < arr.lenght; i++) {
+  //   if (arr[i] % 2 === 0) {
+  //     newArray.push(arr[i])
+  //   }
+  // }
+  arr.filter((numb) => (numb % 2 == 0 ? newArray.push(numb) : false))
   return newArray
 }
 
